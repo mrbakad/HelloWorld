bf988466144f7071e4a5e1ae407fc417c3235706
mrbakad <m1374g@gmail.com>
2025-01-21 14:08:36 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-<<<<<<< HEAD
+
         System.out.println("Hello, World!");
-=======
-        System.out.printf("Hello, World!");
->>>>>>> 07f63bc5d0f202a7874e0263b0326065b38a440d
+
     }
 }
